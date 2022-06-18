d6e5ac5b44c734d41f88fbddc73ef284504464fa
AbhishekJuyal <52237237+AbhishekJuyal@users.noreply.github.com>
2022-06-18 16:59:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bp.json());
 require('./routes/taskRoute')(app);
 require('./routes/userRoute')(app);
 
-mongoUri = 'mongodb://abhijuyal:KKhwgzjbXUGzZqVN@cluster0-shard-00-00.misbj.mongodb.net:27017,cluster0-shard-00-01.misbj.mongodb.net:27017,cluster0-shard-00-02.misbj.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-magpb1-shard-0&authSource=admin&retryWrites=true&w=majority',
+mongoUri = '',
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoUri);
